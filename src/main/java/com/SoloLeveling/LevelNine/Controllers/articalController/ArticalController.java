8775cc30129ce9dba82f85dbf9a3d466f839c012
Sanjay Devarjankar <sanjay.d@medtrixhealthcare.com>
2025-11-24 19:27:20 +0530

    started with controller

---
+++ src/main/java/com/SoloLeveling/LevelNine/Controllers/articalController/ArticalController.java
@@ -0,0 +1,12 @@
+package com.SoloLeveling.LevelNine.Controllers.articalController;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/artical")
+public class ArticalController {
+
+
+}
