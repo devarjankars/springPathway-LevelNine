75fdaecc4bede6af7ae553a35dab693141a0a2f3
Sanjay Devarjankar <sanjay.d@medtrixhealthcare.com>
2025-12-05 15:06:16 +0530

    added controller for like feture

--- src/main/java/com/SoloLeveling/LevelNine/Controllers/LikeController/LikeController.java
+++ src/main/java/com/SoloLeveling/LevelNine/Controllers/LikeController/LikeController.java
@@ -1,4 +1,54 @@
 package com.SoloLeveling.LevelNine.Controllers.LikeController;
 
+
+import com.SoloLeveling.LevelNine.DTOs.LikeRequestDTO;
+import com.SoloLeveling.LevelNine.DTOs.LikeResponseDTO;
+import com.SoloLeveling.LevelNine.Service.LikeService.LikeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/api/v1")
 public class LikeController {
+
+    private  final LikeService likeService;
+     public  LikeController(LikeService likeService){
+         this.likeService=likeService;
+     }
+
+     @PostMapping("/article/{articleId}/toggleLike")
+    public ResponseEntity<?> ToggleLikeBtnApi(@PathVariable Long articleId){
+         try{
+
+             LikeResponseDTO likeResponseDTO= likeService.toggleRequest(articleId);
+             return ResponseEntity.status(201).body(likeResponseDTO);
+         }  catch (IllegalStateException e) {
+             return ResponseEntity.status(400).body(e.getMessage());
+         }catch (NoSuchElementException e) {
+             return ResponseEntity.status(404).body(e.getMessage());
+         }
+         catch (Exception e) {
+             return ResponseEntity.status(500).body(e.getMessage());
+         }
+     }
+
+      @GetMapping("/article/{articleId}/Likes")
+      public ResponseEntity<?> getLikes(@PathVariable Long articleId){
+          try{
+              LikeResponseDTO likeResponseDTO= likeService.getLikedCount(articleId);
+              return ResponseEntity.status(201).body(likeResponseDTO);
+
+          }  catch (IllegalStateException e) {
+              return ResponseEntity.status(400).body(e.getMessage());
+          }catch (NoSuchElementException e) {
+              return ResponseEntity.status(404).body(e.getMessage());
+          }
+          catch (Exception e) {
+              return ResponseEntity.status(500).body(e.getMessage());
+          }
+      }
+
+
 }
--- src/main/java/com/SoloLeveling/LevelNine/Service/LikeService/LikeService.java
+++ src/main/java/com/SoloLeveling/LevelNine/Service/LikeService/LikeService.java
@@ -20,12 +20,17 @@ public class LikeService {
     private  final LikeRepository likeRepository;
     private  final UserRepository userRepository;
     private  final ArticalRepository articalRepository;
+
+
     public LikeService(LikeRepository likeRepository, UserRepository userRepository, ArticalRepository articalRepository){
         this.likeRepository=likeRepository;
         this.userRepository=userRepository;
         this.articalRepository=articalRepository;
     }
 
+
+
+//   toggle Likw btn
     public LikeResponseDTO toggleRequest(Long articleID){
         String currentUsername= SecurityContextHolder.getContext().getAuthentication().getName();
         UserEntity user= userRepository.findByUsername(currentUsername).
@@ -47,6 +52,22 @@ public class LikeService {
         responseDTO.setLiked(true);
         responseDTO.setTotalLikeCount(likeRepository.countByArticle(artical));
         return responseDTO;
+    }
+
+    public LikeResponseDTO getLikedCount(Long articleId){
+
+        String currentUsername= SecurityContextHolder.getContext().getAuthentication().getName();
+        UserEntity user = userRepository.findByUsername(currentUsername)
+                .orElseThrow(()-> new IllegalStateException("Your Not Logged In!"));
+        Artical article= articalRepository.findById(articleId)
+                .orElseThrow(()-> new NoSuchElementException("the Article is not found in database"));
+         Integer LikeCount= likeRepository.countByArticle(article);
+         LikeEntity likeEntity= likeRepository.findByUserAndArticle(user,article)
+                 .orElseThrow(()-> new NoSuchElementException("not found in database"));
+         LikeResponseDTO responseDTO= new LikeResponseDTO();
+         responseDTO.setLiked(true);
+         responseDTO.setTotalLikeCount(LikeCount);
+         return responseDTO;
     }
 
 
